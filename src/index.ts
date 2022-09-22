0f0fd3823258b4e75f98dcf8c3af7543a9831ffd
unknown <yeahbruh199@gmail.com>
2022-09-22 20:27:35 +0700

    Trying puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+const { scrapeProduct } = require('./scraper');
+
 dotenv.config();
 
 const app: Express = express();
@@ -8,8 +10,11 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+  
 });
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  scrapeProduct("https://www.tokopedia.com/lapakmusiman/cincin-pria-cowok-keren-titanium-silver-7?extParam=ivf%3Dfalse%26src%3Dsearch");
 });
