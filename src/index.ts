7b72803c9de9a400b4cb7fd1ee0073aaa17dc8f0
unknown <yeahbruh199@gmail.com>
2022-09-27 18:17:49 +0700

    I dont even know what im doing now

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-const { scrapeProduct } = require('./scraper');
-
 dotenv.config();
 
 const app: Express = express();
@@ -16,5 +14,4 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-  scrapeProduct("https://www.tokopedia.com/lapakmusiman/cincin-pria-cowok-keren-titanium-silver-7");
 });
